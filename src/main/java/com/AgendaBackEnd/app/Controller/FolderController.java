dd9816df2b414d728de061616d7292b9ac6bf591
GianBorlenghi <86668090+GianBorlenghi@users.noreply.github.com>
2022-10-17 11:16:08 -0300

    Update FolderController.java

--- src/main/java/com/AgendaBackEnd/app/Controller/FolderController.java
+++ src/main/java/com/AgendaBackEnd/app/Controller/FolderController.java
@@ -28,7 +28,7 @@ import com.AgendaBackEnd.app.Service.IFolderService;
 import com.AgendaBackEnd.app.Service.IUserService;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = /*"*"*/"http://agenda-borlenghi-gian.web.app/")
 @RequestMapping("/api/folder")
 public class FolderController {
 
